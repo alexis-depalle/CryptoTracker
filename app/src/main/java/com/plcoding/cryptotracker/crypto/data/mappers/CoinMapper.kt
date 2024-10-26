173d478080d6ac91798bc4fa510018edac2bd973
Alexis Dépalle <alexis.depalle@gmail.com>
2024-10-26 15:09:58 +0200

    Implement 3 - Building the Custom Line Chart

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
@@ -24,7 +24,7 @@ fun CoinPriceDto.toCoinPrice(): CoinPrice {
         priceUsd = priceUsd,
         dateTime = Instant
             .ofEpochMilli(time)
-            .atZone(ZoneId.of("UTC"))
+            .atZone(ZoneId.systemDefault())
     )
 }
 
